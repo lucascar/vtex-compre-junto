36650b708849cb06e848c8f677f8bd2a490b22e5
Lucas de Carvalho <contato@lucasdecarvalho.com>
2019-01-13 04:58:41 -0200

    Adicionado função do botão de comprar

--- compre-junto/compre-junto.js
+++ compre-junto/compre-junto.js
@@ -17,71 +17,30 @@ function verifyCP(productId) {
 
 function configCP(productId) {
 
-    var cp = verifyCP(productId);
+    return new Promise(function (resolve, reject) {
 
-    //Está função vai verificar se o produto 1 tem a especificação "Compre Junto" se não tiver, ela apaga o Compre Junto da Página
-    cp.then(function (res) {
-        if (!res) {
-            console.log('Compre Junto: Produto não possui produto para compre junto!');
-            document.querySelector('.compre-junto').remove();
-            return false;
-        } else {
+        var cp = verifyCP(productId);
 
-            var uri1 = "/api/catalog_system/pub/products/search?fq=productId:" + productId;
-            var uri2 = "/api/catalog_system/pub/products/search?fq=productId:" + res;
-
-            var produto1 = fetch(uri1);
-            var produto2 = fetch(uri2);
-
-            var valorTotal = 0;
-
-            //Produto 1 do compre junto
-            produto1.then(function (response) {
-                return response.json();
-            }).then(function (response) {
-
-                var produto = response[0]
-                var qtd = 0;
-
-                produto.items.forEach(function (element) {
-                    qtd += element.sellers[0].commertialOffer.AvailableQuantity;
-                });
-                if (qtd <= 0) {
-                    console.log('Compre Junto: Produto 1 sem estoque!');
+        //Está função vai verificar se o produto 1 tem a especificação "Compre Junto" se não tiver, ela apaga o Compre Junto da Página
+        cp.then(function (res) {
+            if (!res) {
+                console.log('Compre Junto: Produto não possui produto para compre junto!');
+                if (document.querySelector('.compre-junto')) {
                     document.querySelector('.compre-junto').remove();
-                    return false;
                 }
+                resolve(null);
+                return false;
+            } else {
 
-                var configProd = {
-                    productName: produto.productName,
-                    productImg: produto.items[0].images[0].imageUrl,
-                    productPrices: {
-                        priceDe: produto.items[0].sellers[0].commertialOffer.ListPrice,
-                        pricePor: produto.items[0].sellers[0].commertialOffer.Price
-                    },
-                    productTams: {}
-                }
+                var uri1 = "/api/catalog_system/pub/products/search?fq=productId:" + productId;
+                var uri2 = "/api/catalog_system/pub/products/search?fq=productId:" + res;
 
-                produto.items.forEach(function (val) {
-                    configProd.productTams[val.TAMANHO[0]] = val.itemId;
-                })
+                var produto1 = fetch(uri1);
+                var produto2 = fetch(uri2);
+                var produtosCP = [];
 
-                document.querySelector('.compre-junto').setAttribute('style', 'display: block;');
-                document.querySelector('.compre-junto .produto_1 img').setAttribute('src', configProd.productImg);
-                document.querySelector('.compre-junto .produto_1 .nome').innerHTML = configProd.productName;
-                document.querySelector('.compre-junto .produto_1 .prec .antigo').innerHTML = `R$ ${getPrice(configProd.productPrices.priceDe)}`;
-                document.querySelector('.compre-junto .produto_1 .prec .atual').innerHTML = `R$ ${getPrice(configProd.productPrices.pricePor)}`;
-
-                var select = document.querySelectorAll('.tams select')[0];
-
-                for (var i1 = 0; i1 < Object.keys(configProd.productTams).length; i1++) {
-                    select.appendChild(new Option(Object.keys(configProd.productTams)[i1], Object.values(configProd.productTams)[i1]))
-                }
-
-                valorTotal += configProd.productPrices.pricePor;
-
-                //Produto 2 do compre junto
-                produto2.then(function (response) {
+                //Produto 1 do compre junto
+                produto1.then(function (response) {
                     return response.json();
                 }).then(function (response) {
 
@@ -92,8 +51,11 @@ function configCP(productId) {
                         qtd += element.sellers[0].commertialOffer.AvailableQuantity;
                     });
                     if (qtd <= 0) {
-                        console.log('Compre Junto: Produto 2 sem estoque!');
-                        document.querySelector('.compre-junto').remove();
+                        console.log('Compre Junto: Produto 1 sem estoque!');
+                        if (document.querySelector('.compre-junto')) {
+                            document.querySelector('.compre-junto').remove();
+                        }
+                        resolve(null);
                         return false;
                     }
 
@@ -111,28 +73,97 @@ function configCP(productId) {
                         configProd.productTams[val.TAMANHO[0]] = val.itemId;
                     })
 
-                    document.querySelector('.compre-junto').setAttribute('style', 'display: block;');
-                    document.querySelector('.compre-junto .produto_2 img').setAttribute('src', configProd.productImg);
-                    document.querySelector('.compre-junto .produto_2 .nome').innerHTML = configProd.productName;
-                    document.querySelector('.compre-junto .produto_2 .prec .antigo').innerHTML = `R$ ${getPrice(configProd.productPrices.priceDe)}`;
-                    document.querySelector('.compre-junto .produto_2 .prec .atual').innerHTML = `R$ ${getPrice(configProd.productPrices.pricePor)}`;
+                    produtosCP.push(configProd);
 
-                    var select = document.querySelectorAll('.tams select')[1];
+                    //Produto 2 do compre junto
+                    produto2.then(function (response) {
+                        return response.json();
+                    }).then(function (response) {
 
-                    for (var i1 = 0; i1 < Object.keys(configProd.productTams).length; i1++) {
-                        select.appendChild(new Option(Object.keys(configProd.productTams)[i1], Object.values(configProd.productTams)[i1]))
-                    }
+                        var produto = response[0]
+                        var qtd = 0;
 
-                    valorTotal += configProd.productPrices.pricePor;
+                        produto.items.forEach(function (element) {
+                            qtd += element.sellers[0].commertialOffer.AvailableQuantity;
+                        });
+                        if (qtd <= 0) {
+                            console.log('Compre Junto: Produto 2 sem estoque!');
+                            if (document.querySelector('.compre-junto')) {
+                                document.querySelector('.compre-junto').remove();
+                            }
+                            resolve(null);
+                            return false;
+                        }
 
-                    //Insere o valor total dos 2 produtos
-                    document.querySelector('.coprjt-total').innerHTML = `R$ ${getPrice(valorTotal)}`;
+                        var configProd = {
+                            productName: produto.productName,
+                            productImg: produto.items[0].images[0].imageUrl,
+                            productPrices: {
+                                priceDe: produto.items[0].sellers[0].commertialOffer.ListPrice,
+                                pricePor: produto.items[0].sellers[0].commertialOffer.Price
+                            },
+                            productTams: {}
+                        }
+
+                        produto.items.forEach(function (val) {
+                            configProd.productTams[val.TAMANHO[0]] = val.itemId;
+                        })
+
+                        produtosCP.push(configProd);
+                        produtosCP.push(true)
+
+                        resolve(produtosCP);
+
+                    });
 
                 });
-
-            });
-        }
+            }
+        })
     })
 }
 
-configCP(dataLayer[0]['productId'])
+function createCP() {
+    configCP(dataLayer[0]['productId']).then(function (r) {
+        if (!r) {
+            return false;
+        } else {
+
+            var finalPrice = 0;
+
+            for (var i = 0; i < 2; i++) {
+
+                document.querySelector('.compre-junto').setAttribute('style', 'display: block;');
+                document.querySelector('.compre-junto .produto_' + (i + 1) + ' img').setAttribute('src', r[i].productImg);
+                document.querySelector('.compre-junto .produto_' + (i + 1) + ' .nome').innerHTML = r[i].productName;
+                document.querySelector('.compre-junto .produto_' + (i + 1) + ' .prec .antigo').innerHTML = `R$ ${getPrice(r[i].productPrices.priceDe)}`;
+                document.querySelector('.compre-junto .produto_' + (i + 1) + ' .prec .atual').innerHTML = `R$ ${getPrice(r[i].productPrices.pricePor)}`;
+
+                var select = document.querySelectorAll('.tams select')[i];
+
+                for (var i1 = 0; i1 < Object.keys(r[i].productTams).length; i1++) {
+                    select.appendChild(new Option(Object.keys(r[i].productTams)[i1], Object.values(r[i].productTams)[i1]))
+                }
+
+                finalPrice += r[i].productPrices.pricePor;
+            }
+
+            document.querySelector('.coprjt-total').innerHTML = `R$ ${getPrice(finalPrice)}`;
+
+
+            document.querySelector('.compre-junto .btn-compra').addEventListener('click', function () {
+
+                var skuIds = {
+                    prod1: document.querySelectorAll('.tams select')[0].value,
+                    prod2: document.querySelectorAll('.tams select')[1].value
+                }
+
+                if (skuIds.prod1 == 'Tamanho' || skuIds.prod2 == 'Tamanho') {
+                    alert('Por favor, selecione o tamanho!');
+                } else {
+                    window.location.href = "/checkout/cart/add?sc=1&sku=" + skuIds.prod1 + "&qty=1&seller=1" + "&sku=" + skuIds.prod2 + "&qty=1&seller=1";
+                }
+            });
+
+        }
+    })
+}
